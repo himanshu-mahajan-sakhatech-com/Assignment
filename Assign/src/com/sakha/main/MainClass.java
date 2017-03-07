f78bd98eedf30461e1530b77fb8a4bc6fab192c8
skyrbe <harsha.venkatram@sakhatech.com>
2017-03-07 11:58:37 +0530

    1 Commit

---
+++ Assign/src/com/sakha/main/MainClass.java
@@ -0,0 +1,17 @@
+package com.sakha.main;
+
+import java.sql.SQLException;
+
+import com.sakha.login.Login1;
+
+public class MainClass {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		Login1 l = new Login1();
+		l.getUser();
+		
+
+	}
+
+}
